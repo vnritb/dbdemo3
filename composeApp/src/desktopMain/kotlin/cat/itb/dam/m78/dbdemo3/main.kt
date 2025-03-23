f918a33b59bfc7c3c8497395bc2f9b3416519265
vnritb <victor.naranjo@itb.cat>
2025-03-23 18:31:01 +0100

    Initial version

--- composeApp/src/desktopMain/kotlin/cat/itb/dam/m78/dbdemo3/main.kt
+++ composeApp/src/desktopMain/kotlin/cat/itb/dam/m78/dbdemo3/main.kt
@@ -1,12 +1,15 @@
 package cat.itb.dam.m78.dbdemo3
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "dbdemo3",
+        state = WindowState(width = 422.dp, height = 800.dp)
     ) {
         App()
     }
