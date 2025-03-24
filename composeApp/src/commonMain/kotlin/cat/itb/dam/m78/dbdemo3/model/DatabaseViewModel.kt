2646b2e1a790bca0d4f845513ec7a412c5aa62eb
vnritb <victor.naranjo@itb.cat>
2025-03-24 22:05:50 +0100

    Agafar els objectes sencers MyTable, i no només el text

--- composeApp/src/commonMain/kotlin/cat/itb/dam/m78/dbdemo3/model/DatabaseViewModel.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/dam/m78/dbdemo3/model/DatabaseViewModel.kt
@@ -3,12 +3,12 @@ package cat.itb.dam.m78.dbdemo3.model
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import cat.itb.dam.m78.dbdemo3.db.MyTable
 
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
 class DatabaseViewModel : ViewModel() {
-    val allTexts = mutableStateOf<List<String>>(emptyList())
+    val allTexts = mutableStateOf<List<MyTable>>(emptyList())
 
     init {
         fetchAllTexts()
