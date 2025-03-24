7b4184b094dda1480034c9ab7163a08c6be497da
vnritb <victor.naranjo@itb.cat>
2025-03-24 20:44:55 +0100

    Modificar l'ap per que mostri un allista amb tots els elements de la taula i un botó per esborrar, que encara no funciona

--- composeApp/src/commonMain/kotlin/cat/itb/dam/m78/dbdemo3/view/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/dam/m78/dbdemo3/view/App.kt
@@ -1,12 +1,18 @@
 package cat.itb.dam.m78.dbdemo3.view
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cat.itb.dam.m78.dbdemo3.model.database
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -15,14 +21,26 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun App() {
     MaterialTheme {
         val myTableQueries = database.myTableQueries
-        //myTableQueries.insert("some Text")
+        //myTableQueries.insert(4,"Hello, world!")
         val all = myTableQueries.selectAll().executeAsList()
         //val one = myTableQueries.find(1).executeAsOneOrNull()
-
-
         var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(all[0])
+            all.forEach { text ->
+                Row (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                        .background(MaterialTheme.colors.surface, shape = RoundedCornerShape(8.dp))
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text)
+                    IconButton(onClick = { println("pulsado") }) {
+                        Icon(Icons.Default.Delete, contentDescription = "Delete")
+                    }
+                }            }
         }
     }
 }
