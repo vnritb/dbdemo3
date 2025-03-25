e6291066baa86ec4185661f5a8f2dc3c2e62e78c
vnritb <victor.naranjo@itb.cat>
2025-03-25 11:16:51 +0100

    Bdd en runtime en common apps (para desarrollo)

--- composeApp/src/desktopMain/kotlin/cat/itb/dam/m78/dbdemo3/model/DatabaseMigraionHelper.kt
+++ composeApp/src/desktopMain/kotlin/cat/itb/dam/m78/dbdemo3/model/DatabaseMigraionHelper.kt
@@ -2,15 +2,24 @@ package cat.itb.dam.m78.dbdemo3.model
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import cat.itb.dam.m78.dbdemo3.db.Database
 import kotlin.io.path.Path
 import kotlin.io.path.absolutePathString
 
 actual fun createDriver(): SqlDriver {
+
+    val currentDir = System.getProperty("user.dir")
+    println("Current directory: $currentDir")
+
     val userHome = System.getProperty("user.home")
-    val file = Path(userHome, "myDatabase.db")
+    println("userHome: $userHome")
+
+    //val file = Path(userHome, "myDatabase.db")
+    val file = Path(currentDir, "myDatabase.db")
     val driver = JdbcSqliteDriver("jdbc:sqlite:${file.absolutePathString()}")
-    //println(file.absolutePathString())
-    //Database.Schema.create(driver)
+
+    println(file.absolutePathString())
+    Database.Schema.create(driver)
     return driver
 }
 
