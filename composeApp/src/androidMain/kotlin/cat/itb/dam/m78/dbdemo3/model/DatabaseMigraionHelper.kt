fc13595f7e49ed99b49e2ececfccfa4bd028083c
vnritb <victor.naranjo@itb.cat>
2025-04-03 10:58:52 +0200

    .

--- composeApp/src/androidMain/kotlin/cat/itb/dam/m78/dbdemo3/model/DatabaseMigraionHelper.kt
+++ composeApp/src/androidMain/kotlin/cat/itb/dam/m78/dbdemo3/model/DatabaseMigraionHelper.kt
@@ -5,7 +5,7 @@ import cat.itb.dam.m78.dbdemo3.db.Database
 
 
 actual fun createDriver(): SqlDriver {
-    //val appContext = applicationContex
+    //val appContext = applicationContex.
     //return app.cash.sqldelight.db.(Database.Schema, "myDatabase.db")
     return TODO("Provide the return value")
 }
